ea0f9838d464174451b7e72208fa1056217091fc
Matthew Cavallo <macavall@microsoft.com>
2024-01-08 23:11:59 -0600

    Check isRunning

--- http2.cs
+++ http2.cs
@@ -8,16 +8,20 @@ namespace PerfTestDLLFA
     public class http2
     {
         private readonly ILogger _logger;
+        private readonly IServiceUpdater serviceUpdater;
 
-        public http2(ILoggerFactory loggerFactory)
+        public http2(ILoggerFactory loggerFactory, IServiceUpdater _serviceUpdater)
         {
             _logger = loggerFactory.CreateLogger<http2>();
+            serviceUpdater = _serviceUpdater;
         }
 
         [Function("http2")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            serviceUpdater.StartSender();
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
